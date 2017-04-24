3151fcd8bde7e955027bd0e89c7bf19031ce3f49
JhonnyLi <jhonny@jhonny.se>
2017-04-24 21:37:49 +0200

    Model changes

--- SynchronizedShoppingLists/ShoppingListApp/ShoppingListApp/Models/MainPageViewModel.cs
+++ SynchronizedShoppingLists/ShoppingListApp/ShoppingListApp/Models/MainPageViewModel.cs
@@ -5,14 +5,43 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using Newtonsoft.Json;
+using System.Net.Http.Headers;
 
 namespace ShoppingListApp.Models
 {
     class MainPageViewModel
     {
+        public List<ShoppingList> AllShoppingLists = new List<ShoppingList>();
+        public List<Item> AllItems = new List<Item>();
+
+        private readonly HttpClient _client;
         public MainPageViewModel()
         {
+            _client = new HttpClient();
+            GetAllShoppingLists();
 
+        }
+
+        
+        private async Task<List<ShoppingList>> GetAllShoppingLists()
+        {
+            var Url = "http://sync.jhonny.se/api/Values/";
+            List<ShoppingList> model = new List<ShoppingList>();
+            try
+            {
+                _client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                Task<string> contentsTask = _client.GetStringAsync(Url);
+                string contents = await contentsTask.ConfigureAwait(false);
+                //var respons = JsonConvert.DeserializeObject<List<ShoppingList>>(contents);
+                AllShoppingLists = JsonConvert.DeserializeObject<List<ShoppingList>>(contents);
+                var dummy = "";
+            }
+            catch (Exception ex)
+            {
+                var error = ex.Message;
+                var breakit = "";
+            }
+            return model;
         }
     }
 }
---
+++ SynchronizedShoppingLists/ShoppingListApp/ShoppingListApp/Models/ShoppingList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingListApp.Models
+{
+    public class ShoppingList
+    {
+        public Guid ShoppingListId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Item> Items { get; set; } = new List<Item>();
+    }
+}
